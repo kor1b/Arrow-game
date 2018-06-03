a1009e875d8ed653d7448dafafc2c862114d90fc
kor1b <39777055+kor1b@users.noreply.github.com>
2018-06-03 23:46:56 +0300

    GameOver, реакция кнопок на нажатие
    Настроена структура GameOverMenu, добавлены спрайты к кнопкам. Теперь кнопки по нажатию уменьшаются в размерах

---
+++ Assets/Scripts/MainMenu/ButtonClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonClick : EventTrigger{
+
+	public void OnDown(){
+		transform.localScale = new Vector2 (0.95f, 0.95f);
+	}
+
+	public void OnUp(){
+		transform.localScale = new Vector2 (1, 1);
+	}
+}
